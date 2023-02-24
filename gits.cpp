2386fc9f39af320c30c80548a54f646af4f4bbe6
ash_tasnim_1834 <xcadettasnim1834@gmail.com>
2023-02-24 22:42:56 +0600

    first commit

---
+++ gits.cpp
@@ -0,0 +1,18 @@
+//In The Name Of ALlah, The most Gracious The Most Merciful
+#include<bits/stdc++.h>
+#define ll long long
+#define shoot           ios_base::sync_with_stdio(false); cin.tie(0);
+#define tc             int t; cin>>t;
+#define vi vector<int> 
+#define pp pair<int,int>
+#define cy cout<<"YES"<<"\n"
+#define cn cout<<"NO"<<"\n"
+using namespace std;
+int main()
+{
+shoot;
+int n;
+
+
+return 0;
+}
